a92a56507d0b2ff30659cd5143f73cbb52e2ae17
msi33 <msomowork@gmail.com>
2024-05-17 21:30:53 -0500

    listed names number in sales dept

--- EmployeeSQL/challenge_queries.sql
+++ EmployeeSQL/challenge_queries.sql
@@ -14,9 +14,6 @@ WHERE DATE_PART('year',hire_date) = 1986;
 
 
 --3. List the manager of each department along with their department number, department name, employee number, last name, and first name
-
-SELECT * FROM dept_manager
---List the manager of each department along with their department number, department name, employee number, last name, and first name.
 SELECT dm.dept_no, d.dept_name, dm.emp_no, e.last_name, e.first_name
 FROM dept_manager AS dm
 INNER JOIN departments AS d
@@ -24,6 +21,38 @@ ON dm.dept_no = d.dept_no
 INNER JOIN employees AS e
 ON dm.emp_no = e.emp_no
 ORDER BY dept_no;
+
+
+--4. List the department number for each employee along with that employee’s employee number, last name, first name, and department name.
+
+SELECT e.emp_no,e.last_name,e.first_name,d.dept_name
+FROM employees AS e
+LEFT JOIN dept_emp AS d_e
+ON e.emp_no = d_e.emp_no
+INNER JOIN departments AS d
+ON d_e.dept_no = d.dept_no
+ORDER BY emp_no;
+
+
+--5. List first name, last name, and sex of each employee whose first name is Hercules and whose last name begins with the letter B.
+
+SELECT e.first_name, e.last_name, e.sex 
+FROM employees AS e
+WHERE e.first_name = 'Hercules' AND e.last_name like 'B%'
+ORDER BY last_name;
+
+
+--6 List each employee in the Sales department, including their employee number, last name, and first name.
+SELECT e.emp_no,e.last_name,e.last_name,e.first_name,d.dept_name
+FROM employees AS e
+LEFT JOIN dept_emp AS d_e
+ON e.emp_no = d_e.emp_no
+INNER JOIN departments AS d
+ON d_e.dept_no = d.dept_no
+WHERE d.dept_name = 'Sales';
+
+
+
 
 
 
