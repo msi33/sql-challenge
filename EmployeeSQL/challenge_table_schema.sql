4a07dda7a390da07913f9398d012f96546ff0c1a
msi33 <msomowork@gmail.com>
2024-05-17 19:15:44 -0500

    created six tables

--- EmployeeSQL/challenge_table_schema.sql
+++ EmployeeSQL/challenge_table_schema.sql
@@ -1,8 +1,13 @@
---TABLE departments
-
-CREATE TABLE departments (
-  dept_no VARCHAR(10),
-  dept_name VARCHAR(30) NOT NULL,
-  PRIMARY KEY (dept_no)
+-- Create employees table
+CREATE TABLE employees (
+	emp_no INT PRIMARY KEY NOT NULL,
+	emp_title_id VARCHAR NOT NULL,
+	birth_date DATE NOT NULL,
+	first_name VARCHAR NOT NULL,
+	last_name VARCHAR NOT NULL,
+	sex VARCHAR NOT NULL,
+	hire_date DATE NOT NULL
 );
+
+SELECT * FROM employees
 
