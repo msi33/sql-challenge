ebd30dd6c40ccf333889f8992655172eb4169cec
msi33 <msomowork@gmail.com>
2024-05-17 19:18:36 -0500

    created tables and check select

--- EmployeeSQL/challenge_table_schema.sql
+++ EmployeeSQL/challenge_table_schema.sql
@@ -1,3 +1,4 @@
+
 -- Create employees table
 CREATE TABLE employees (
 	emp_no INT PRIMARY KEY NOT NULL,
@@ -10,4 +11,49 @@ CREATE TABLE employees (
 );
 
 SELECT * FROM employees
+
+-- Create tabel for departments 
+
+CREATE TABLE departments (
+	dept_no VARCHAR PRIMARY KEY NOT NULL,
+	dept_name VARCHAR NOT NULL
+);
+
+SELECT * FROM departments
+
+
+-- Create table for employee department
+create table dept_emp (
+	emp_no INT PRIMARY KEY NOT NULL,
+	dept_no VARCHAR NOT NULL
+);
+
+SELECT * FROM dept_emp
+
+
+-- Create table for department manager 
+create table dept_manager (
+	dept_no VARCHAR NOT NULL,
+	emp_no INT PRIMARY KEY NOT NULL
+);
+
+SELECT * FROM dept_manager 
+
+
+-- Create table for employee title
+create table titles (
+	title_id VARCHAR PRIMARY KEY NOT NULL,
+	title VARCHAR NOT NULL
+);
+
+SELECT * FROM titles 
+
+
+-- Create table for salaries 
+create table salaries (
+	emp_no INT PRIMARY KEY NOT NULL,
+	salary INT NOT NULL
+);
+
+SELECT * FROM salaries
 
